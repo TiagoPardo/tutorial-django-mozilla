58c64151aa347ec1b06ee6a584b6547f65c802ad
Tiago Pardo <tiago.epardo@gmail.com>
2025-05-06 17:22:37 -0300

    Autenticação nativa do Django aplicada e restrição de acesso por permissionamento aplicados

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from catalog.models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     """View function for home page of site."""
@@ -47,3 +48,22 @@ class AuthorListView(generic.ListView):
     
 class AuthorDetailView(generic.DetailView):
     model = Author
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+    
+    
+class AllLoanedBooks(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan of all users."""
+    permission_required = 'catalog.can_mark_returned'
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_librarian.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
